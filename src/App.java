e30ee56351c58356b7bd3bfe64410e4e28db1011
joaoxbarbosa <69282787+joaoxbarbosa@users.noreply.github.com>
2024-09-13 13:46:30 -0300

    sept 13 commit

--- src/App.java
+++ src/App.java
@@ -1,31 +1,201 @@
 import java.util.Scanner;
+import java.util.HashMap; // import the HashMap class
+// from https://www.w3schools.com/java/java_hashmap.asp
 
 public class App {
-    
+
     /**
      * This method is the primary driver method for your program.
      * It should primarily call other methods to do its work.
      * Do NOT edit this method until after you have fully satisfied the reqs for a 5
      */
 
+    Scanner scan = new Scanner(System.in);
+
+    // ansi code from from https://www.geeksforgeeks.org/how-to-print-colored-text-in-java-console/
+    public static final String ANSI_RESET = "\u001B[0m";
+
     public void start() {
-        String userName = getUserName();
-        printGreeting(userName);
-        printInstructions();
-        doMadlib();
-        System.out.println("hi");
+        boolean userWantsToStop = false;
+
+        while (userWantsToStop == false) {
+            printInstructions();
+
+            printGreeting();
+
+            doMadlib();
+
+            // game loop logic below
+
+            System.out.println(returnColoredText("green", "│") + " Do you want to play again? 'Yes' or 'No'");
+
+            System.out.print(returnColoredText("green", "│ ↳") + " ");
+
+            String userResponse = scan.nextLine();
+
+            if (userResponse.toLowerCase().equals("no")) {
+                userWantsToStop = true;
+
+                System.out.println(returnColoredText("green", "│") + " Thanks for playing!");
+            }
+        }
+
+        scan.close();
     }
-    
+
     /* Here you should create all missing methods used by the start() method
      * I have created the signature for ONE of the methods below.
      * You can create any other methods you need as well!
-     */ 
-    
-    public void printInstructions() {
-        
+    */
+
+    /*
+        This is a helper function that will print a prompt to the user
+        and return their input
+
+        Parameters:
+            - scan (type Scanner): the class-wide object for
+            getting user input
+
+            - prompt (type String): the message to display to the user
+
+        Returns:
+            - (type String): the user's input
+    */
+    public String getUserInput() {
+
+        System.out.print(returnColoredText("green", "│ ↳") + " ");
+
+        String input = scan.nextLine();
+
+        return input;
     }
-    
-  
+
+    /*
+        This is a helper function that will print a prompt to the user
+        and return their input
+
+        Parameters:
+            - colorName (type String): the color name for the function to return
+              the color code for
+
+        Returns:
+            - (type String): the ANSI color code
+    */
+    public String getColorCodes(String colorName) {
+
+        // https://stackoverflow.com/questions/8261075/adding-multiple-entries-to-a-hashmap-at-once-in-one-statement
+        // https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
+        HashMap<String, String> colors = new HashMap<String, String>()
+        {{
+            //put("red", "\u001B[31m");
+            //put("yellow", "\u001B[33m");
+            //put("blue", "\u001B[34m");
+            //put("purple", "\u001B[35m");
+            //put("cyan", "\u001B[36m");
+            //put("white", "\u001B[37m");
+            put("green", "\u001B[32m");
+        }};
+
+        return colors.get(colorName);
+    }
+
+    public void printColoredLn(String colorName, String message) {
+        System.out.println(getColorCodes(colorName) + message + ANSI_RESET);
+    }
+
+    public String returnColoredText(String colorName, String message) {
+        return getColorCodes(colorName) + message + ANSI_RESET;
+    }
+
+    public void printInstructions() {
+        // ASCII art from https://patorjk.com/software/taag/#p=display&f=RubiFont&t=Madlibs
+        printColoredLn("green", """
+╭──────────────────────────────────────╮
+│                                      │
+│ ▗▖  ▗▖ ▗▄▖ ▗▄▄▄ ▗▖   ▗▄▄▄▖▗▄▄▖  ▗▄▄▖ │
+│ ▐▛▚▞▜▌▐▌ ▐▌▐▌  █▐▌     █  ▐▌ ▐▌▐▌    │
+│ ▐▌  ▐▌▐▛▀▜▌▐▌  █▐▌     █  ▐▛▀▚▖ ▝▀▚▖ │
+│ ▐▌  ▐▌▐▌ ▐▌▐▙▄▄▀▐▙▄▄▖▗▄█▄▖▐▙▄▞▘▗▄▄▞▘ │
+│                                      │
+╰──────────────────────────────────────╯""");
+
+        // Unicode symbols for formatting looks like https://www.w3.org/TR/xml-entity-names/025.html
+        printColoredLn("green", "╭────────────────╮");
+        printColoredLn("green", "│  Instructions  │");
+        printColoredLn("green", "├────────────────╯"); //  ╰
+
+        // bullet point icon from  https://www.merriam-webster.com/grammar/how-to-use-bullet-points
+        System.out.println(returnColoredText("green", "│") + " • Enter your name");
+        System.out.println(returnColoredText("green", "│") + " • Enter the prompted words to create a funny story!");
+        System.out.println(returnColoredText("green", "│") + " • Repeat until you're tired of laughing");
+    }
+
+    public void printGreeting() {
+        printColoredLn("green","├──────────────────────────╮"); // ╭
+        printColoredLn("green","│    Welcome to Madlibs    │");
+        printColoredLn("green","├──────────────────────────╯"); //  ╰
+        System.out.println(returnColoredText("green", "│") + " What is your name?");
+
+        String userName = getUserInput();
+
+        printColoredLn("green","├────────────────────");
+        System.out.println(returnColoredText("green", "│") + " Hello, " + returnColoredText("green", userName) + "!");
+        System.out.println(returnColoredText("green", "│") + " Let's play a game!");
+    }
+
+    public void doMadlib() {
+        // I'll create an array of the terms I need, and loop
+        // through it to create a map of terms to the user's input
+
+        // https://www.w3schools.com/java/java_arrays.asp
+        String[] terms = {
+            "country",
+            "noun",
+            "adjective",
+            "profession",
+            "name",
+            "tool",
+            "family member",
+            "animal",
+            "color",
+            "plural noun",
+            "place"
+        };
+
+        HashMap<String, String> userInputs = new HashMap<String, String>();
+
+        printColoredLn("green","├────────────────────");
+        System.out.println(returnColoredText("green", "│") + " Please enter the following:");
+
+        // https://www.w3schools.com/java/java_foreach_loop.asp
+        for (String term : terms) {
+            System.out.println(returnColoredText("green", "│") + " Enter a " + returnColoredText("green", term));
+
+            String userInput = getUserInput();
+
+            userInputs.put(term, userInput);
+        }
+
+        String story = "There was once an epic [profession] from [country] who moved\n" +
+            returnColoredText("green", "│") + " " + "to a [place]. They were known for their [adjective] [noun]\n" +
+            returnColoredText("green", "│") + " " + "and their trusty [tool]. They lived with [family member]\n" +
+            returnColoredText("green", "│") + " " + "and their pet [animal]. They loved the color [color] and\n" +
+            returnColoredText("green", "│") + " " + "were known by the pseudonym [name]. They were always\n" +
+            returnColoredText("green", "│") + " " + "seen wearing [color] [plural noun].";
+
+        // https://www.w3schools.com/java/java_foreach_loop.asp (same as above)
+        // looping through the array of terms, creating a variable of type String
+        // and saving it to a variable named "term"
+        for (String term : terms) {
+            story = story.replace("[" + term + "]", returnColoredText("green", userInputs.get(term)));
+        }
+
+        printColoredLn("green", "├────────────────────");
+        System.out.println(returnColoredText("green", "│") + " Here is your story:");
+        printColoredLn("green","├────────────────────");
+        System.out.println(returnColoredText("green", "│") + " " + story);
+        printColoredLn("green", "╰────────────────────");
+    }
 
     /* DO NOT TOUCH OR EDIT THE METHOD BELOW! ADD NO CODE BELOW THIS LINE */
     public static void main(String[] args) throws Exception {
